2d80f8d591711396ff24c435488f870e4298a2c2
arthurlefevre <lefevre.arthur@outlook.fr>
2023-02-20 08:37:50 +0100

    test

--- workflow/src/functions/helloWorld.ts
+++ workflow/src/functions/helloWorld.ts
@@ -4,5 +4,6 @@ import randomNumber from "./randomNumber";
  * @returns Promise<string>
  */
 export default function helloWorld(): string {
+  console.log("Helloworld");
   return `Hello world : ${randomNumber()}`;
 }
